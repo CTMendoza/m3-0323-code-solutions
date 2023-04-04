5dc8d3ba1461e280bfa8e3f90865bbb5a07bdd0d
Joe King <tiptop1112@gmail.com>
2023-04-04 04:36:10 +0000

    Omitted console.log in PokemonList.js component

--- react-rendering-lists/my-app/src/PokemonList.js
+++ react-rendering-lists/my-app/src/PokemonList.js
@@ -1,6 +1,4 @@
-
 export default function PokemonList ({pokeArray}) {
-  console.log('pokeArray: ', pokeArray)
   const listPokemon = pokeArray.map(pokemon =>
     <li key={pokemon.number}>{pokemon.name}</li>
     );
